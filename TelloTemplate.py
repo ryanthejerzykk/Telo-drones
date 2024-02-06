f1c35468475e166c8bedaf4025463c099980156f
jerzyk <ryanjerzyk@gmail.com>
2024-02-06 08:02:38 -0500

    changed names and date

--- TelloTemplate.py
+++ TelloTemplate.py
@@ -44,9 +44,9 @@ recvThread.start()
 # CREATE FUNCTIONS HERE....
 
 
-print("\nFirst & Last Names")
-print("Program Name: ")
-print("Date: ")
+print("\nRyan Jerzyk/Christopher Melbow")
+print("Program Name:Autonomous  ")
+print("Date:2.6.24 ")
 print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
 print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
 ready = input('\nAre you ready to take flight: ')
